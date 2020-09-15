929cc015cb69efa304eb933fea79aa523c208edb
Anex <andrii.levchenko.mil@gmail.com>
2020-09-15 13:40:11 +0300

    base game commit 1\4

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    int currentScene;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+    
+    void Start()
+    {
+        
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+        StartCoroutine(LoadNextLevel());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        int nextSceneIndex = currentScene + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
